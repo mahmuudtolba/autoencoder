337badf9f1e565f6384e4bd68780148a10b1ea7e
Mahmoud Tolba <mahmuudtolba@gmail.com>
2022-12-26 10:08:10 +0200

    starting the repo

---
+++ autoencoder.py
@@ -0,0 +1,76 @@
+from tensorflow import keras
+from keras import layers
+import tensorflow as tf
+import numpy as np
+
+
+class Sampler(layers.Layer):
+    def call(self, z_mean, z_log_var):
+        batch_size = tf.shape(z_mean)[0]
+        z_size = tf.shape(z_mean)[1]
+        epsilon = tf.random.normal(shape=(batch_size, z_size))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+class VAE(keras.Model):
+    def __init__(self,latent_dim , input_shape_ , **kwargs):
+        super().__init__(**kwargs)
+        self.sampler = Sampler()
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss")
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        self.latent_dim =  latent_dim
+        self.input_shape_ = input_shape_
+        self.encoder =self.encoder_()
+        self.decoder= self.decoder_()
+
+    def encoder_(self):
+        encoder_inputs = keras.Input(shape=self.input_shape_)
+        x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+        x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+        self.shape_before_flatten = x.shape.as_list()[1:]
+        x = layers.Flatten()(x)
+        x = layers.Dense(16, activation="relu")(x)
+        z_mean = layers.Dense(self.latent_dim, name="z_mean")(x)
+        z_log_var = layers.Dense(self.latent_dim, name="z_log_var")(x)
+        return keras.Model(encoder_inputs, [z_mean, z_log_var], name="encoder")
+
+    def decoder_(self):
+        latent_inputs = keras.Input(shape=(self.latent_dim,))
+        x = layers.Dense(np.prod(self.shape_before_flatten), activation="relu")(latent_inputs)
+        x = layers.Reshape(self.shape_before_flatten)(x)
+        x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+        x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+        decoder_outputs = layers.Conv2D(1, 3, activation="sigmoid", padding="same")(x)
+        return keras.Model(latent_inputs, decoder_outputs, name="decoder")
+
+
+    @property
+    def metrics(self):
+        return [self.total_loss_tracker,
+                self.reconstruction_loss_tracker,
+                self.kl_loss_tracker]
+
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var = self.encoder(data)
+            z = self.sampler(z_mean, z_log_var)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.binary_crossentropy(data, reconstruction),
+                    axis=(1, 2)
+                )
+            )
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            total_loss = reconstruction_loss + tf.reduce_mean(kl_loss)
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "total_loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
